538bf83c0905e6a19f394dd5ebd904ec4d3686f6
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-18 05:42:32 +0700

    change payment status to using enum

--- internal/billing/domain/schedule.go
+++ internal/billing/domain/schedule.go
@@ -1,19 +1,20 @@
 package domain
 
 import (
+	"billing-engine/pkg/enum"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
 )
 
 type Schedule struct {
-	ScheduleID     uuid.UUID `json:"schedule_id" gorm:"type:uuid;primaryKey"`
-	LoanID         uuid.UUID `json:"loan_id" gorm:"type:uuid"`
-	PaymentNo      int       `json:"payment_no"`
-	PaymentDueDate time.Time `json:"payment_due_date"`
-	PaymentAmount  float64   `json:"payment_amount"`
-	PaymentStatus  string    `json:"payment_status"`
-	IsMissPayment  bool      `json:"is_miss_payment"`
+	ScheduleID     uuid.UUID          `json:"schedule_id" gorm:"type:uuid;primaryKey"`
+	LoanID         uuid.UUID          `json:"loan_id" gorm:"type:uuid"`
+	PaymentNo      int                `json:"payment_no"`
+	PaymentDueDate time.Time          `json:"payment_due_date"`
+	PaymentAmount  float64            `json:"payment_amount"`
+	PaymentStatus  enum.PaymentStatus `json:"payment_status"`
+	IsMissPayment  bool               `json:"is_miss_payment"`
 	AuditLog
 
 	Loan Loan `json:"loan" gorm:"foreignKey:LoanID"`
