99fec74349f2e9603b361e0119088d421fe706f3
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-18 05:42:47 +0700

    add model for billing domain

---
+++ internal/billing/model/billing.go
@@ -0,0 +1,49 @@
+package model
+
+import (
+	"billing-engine/pkg/enum"
+	"github.com/google/uuid"
+)
+
+type CreateLoanPayload struct {
+	CustomerID uuid.UUID `json:"customer_id"`
+}
+
+type ScheduleResponse struct {
+	ScheduleID     uuid.UUID          `json:"schedule_id"`
+	LoanID         uuid.UUID          `json:"loan_id"`
+	PaymentNo      int                `json:"payment_no"`
+	PaymentDueDate string             `json:"payment_due_date"`
+	PaymentAmount  float64            `json:"payment_amount"`
+	PaymentStatus  enum.PaymentStatus `json:"payment_status"`
+	IsMissPayment  bool               `json:"is_miss_payment"`
+}
+
+type CreateLoanResponse struct {
+	Schedules []ScheduleResponse `json:"schedules"`
+}
+
+type GetScheduleResponse struct {
+	Schedules []ScheduleResponse `json:"schedules"`
+}
+
+type IsDelinquentPayload struct {
+	LoanID     uuid.UUID `query:"loan_id"`
+	CustomerID uuid.UUID `query:"customer_id"`
+}
+
+type IsDelinquentResponse struct {
+	IsDelinquent bool `json:"is_delinquent"`
+}
+
+type GetCustomerResponse struct {
+	CustomerID  uuid.UUID `json:"customer_id"`
+	FirstName   string    `json:"first_name"`
+	LastName    string    `json:"last_name"`
+	Email       string    `json:"email"`
+	PhoneNumber string    `json:"phone_number"`
+}
+
+type CreateCustomerPayload struct {
+	TotalCustomer int `json:"total_customer"`
+}
