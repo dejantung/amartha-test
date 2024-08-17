b16cfbe76dafe835a6f35edca8da443599711b78
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-18 05:49:32 +0700

    refactor package name custom error

---
+++ pkg/customerror/error.go
@@ -0,0 +1,28 @@
+package customerror
+
+import "fmt"
+
+type Cause string
+
+const (
+	InvalidInput  Cause = "INVALID_INPUT"
+	InternalError Cause = "INTERNAL_ERROR"
+	NotFound      Cause = "NOT_FOUND"
+	AlreadyExists Cause = "ALREADY_EXISTS"
+)
+
+type CustomError struct {
+	Cause Cause
+	Msg   string
+}
+
+func New(cause Cause, msg string) *CustomError {
+	return &CustomError{
+		Cause: cause,
+		Msg:   msg,
+	}
+}
+
+func (ce *CustomError) Error() string {
+	return fmt.Sprintf("cause: %s, msg: %s", ce.Cause, ce.Msg)
+}
