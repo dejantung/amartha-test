5580b0477f55c5b4e8e3118b393a3ff493987414
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-18 20:40:23 +0700

    add new cmd for serve api server

---
+++ cmd/billing/api-server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"billing-engine/internal/billing/app/server"
+	"billing-engine/pkg/config"
+	"billing-engine/pkg/logger"
+)
+
+func main() {
+	log := logger.NewZeroLogger("billing")
+
+	cfg, err := config.NewConfig("billing")
+	if err != nil {
+		log.WithField("error", err).Error("failed to load config")
+	}
+
+	log.WithField("config", cfg).Info("config loaded successfully")
+	newApiServer, err := server.NewServer(log, cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		if err := newApiServer.Echo.Start(cfg.AppServer.Port); err != nil {
+			log.WithField("error", err).Error("failed to start server")
+		}
+	}()
+
+	newApiServer.Stop()
+}
