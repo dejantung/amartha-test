b0306dae1c87a7df907927a11392d20f9014e626
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-18 22:41:00 +0700

    fix relation

--- internal/billing/domain/loan.go
+++ internal/billing/domain/loan.go
@@ -7,16 +7,15 @@ import (
 )
 
 type Loan struct {
-	LoanID          uuid.UUID `json:"loan_id"`
-	CustomerID      uuid.UUID `json:"customer_id"`
+	LoanID          uuid.UUID `json:"loan_id" gorm:"type:uuid;primaryKey"`
+	CustomerID      uuid.UUID `json:"customer_id" gorm:"type:uuid"`
 	PrincipalAmount float64   `json:"principal_amount"`
 	InterestRate    float64   `json:"interest_rate"`
 	StartDate       time.Time `json:"start_date"`
 	EndDate         time.Time `json:"end_date"`
 	IsFinish        bool      `json:"is_finish"`
 
-	Customer  Customer   `json:"customer" gorm:"foreignKey:CustomerID"`
-	Schedules []Schedule `json:"schedules" gorm:"foreignKey:LoanID"`
+	Schedules []Schedule `json:"schedules" gorm:"foreignKey:LoanID;references:LoanID"`
 	AuditLog
 }
 
--- internal/billing/domain/schedule.go
+++ internal/billing/domain/schedule.go
@@ -9,15 +9,13 @@ import (
 
 type Schedule struct {
 	ScheduleID     uuid.UUID          `json:"schedule_id" gorm:"type:uuid;primaryKey"`
-	LoanID         uuid.UUID          `json:"loan_id" gorm:"type:uuid"`
+	LoanID         uuid.UUID          `json:"loan_id" gorm:"type:uuid;not null"`
 	PaymentNo      int                `json:"payment_no"`
 	PaymentDueDate time.Time          `json:"payment_due_date"`
 	PaymentAmount  float64            `json:"payment_amount"`
 	PaymentStatus  enum.PaymentStatus `json:"payment_status"`
 	IsMissPayment  bool               `json:"is_miss_payment"`
 	AuditLog
-
-	Loan Loan `json:"loan" gorm:"foreignKey:LoanID"`
 }
 
 func (schedule *Schedule) BeforeCreate(tx *gorm.DB) (err error) {
