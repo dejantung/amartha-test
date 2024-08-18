7861646901362bb580bc6de9f1b5635b14cbd989
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-19 00:14:53 +0700

    add model data for payload and response payment service

---
+++ internal/payment/model/payment.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"billing-engine/pkg/enum"
+	"github.com/google/uuid"
+	"time"
+)
+
+type ProcessPaymentPayload struct {
+	Amount     float64   `json:"amount"`
+	LoanID     uuid.UUID `json:"loan_id"`
+	ScheduleID uuid.UUID `json:"schedule_id"`
+}
+
+type ProcessPaymentResponse struct {
+	AmountPaid    float64            `json:"amount_paid"`
+	PaymentID     uuid.UUID          `json:"payment_id"`
+	PaymentStatus enum.PaymentStatus `json:"payment_status"`
+	PaymentDate   string             `json:"payment_date"`
+}
+
+type PaymentEventPayload struct {
+	LoanID        uuid.UUID          `json:"loan_id"`
+	ScheduleID    uuid.UUID          `json:"schedule_id"`
+	PaymentID     uuid.UUID          `json:"payment_id"`
+	PaymentStatus enum.PaymentStatus `json:"payment_status"`
+	PaymentDate   time.Time          `json:"payment_date"`
+}
