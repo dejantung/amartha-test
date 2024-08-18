1503d27b21e76e26475870c235a6b442f27edffa
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-19 00:36:25 +0700

    regenerate mock

--- internal/payment/mocks/mock_payment_repository.go
+++ internal/payment/mocks/mock_payment_repository.go
@@ -43,11 +43,12 @@ func (m *MockPaymentRepositoryProvider) EXPECT() *MockPaymentRepositoryProviderM
 }
 
 // CreateLoan mocks base method.
-func (m *MockPaymentRepositoryProvider) CreateLoan(arg0 context.Context, arg1 domain.Loan) error {
+func (m *MockPaymentRepositoryProvider) CreateLoan(arg0 context.Context, arg1 domain.Loan) (domain.Loan, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "CreateLoan", arg0, arg1)
-	ret0, _ := ret[0].(error)
-	return ret0
+	ret0, _ := ret[0].(domain.Loan)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
 }
 
 // CreateLoan indicates an expected call of CreateLoan.
