dc5a7c2c355737a0b04c5baa2c70c68011a1844b
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-18 09:15:55 +0700

    add cache key in constant

--- internal/billing/constant/billing.go
+++ internal/billing/constant/billing.go
@@ -1,6 +1,8 @@
 package constant
 
 const (
-	INTEREST_RATE = 0.1
-	MAX_PAYMENT   = 50
+	INTEREST_RATE         = 0.1
+	MAX_PAYMENT           = 50
+	CACHE_KEY_DELIQUENCY  = "deliquency:%s"
+	CACHE_KEY_OUTSTANDING = "outstanding:%s"
 )
