683835c5b4a3a4b109a54bb2a2efdd0e0627716a
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-19 00:29:28 +0700

    change data type payment status to using enum

--- internal/payment/domain/payment.go
+++ internal/payment/domain/payment.go
@@ -1,17 +1,18 @@
 package domain
 
 import (
+	"billing-engine/pkg/enum"
 	"github.com/google/uuid"
 	"time"
 )
 
 type Payment struct {
 	Base
-	PaymentID     uuid.UUID `json:"payment_id" gorm:"type:uuid;primaryKey"`
-	LoanID        uuid.UUID `json:"loan_id" gorm:"type:uuid"`
-	ScheduleID    uuid.UUID `json:"schedule_id" gorm:"type:uuid"`
-	PaymentDate   time.Time `json:"payment_date"`
-	AmountPaid    float64   `json:"amount_paid"`
-	PaymentMethod string    `json:"payment_method"`
-	PaymentStatus string    `json:"payment_status"`
+	PaymentID     uuid.UUID          `json:"payment_id" gorm:"type:uuid;primaryKey"`
+	LoanID        uuid.UUID          `json:"loan_id" gorm:"type:uuid"`
+	ScheduleID    uuid.UUID          `json:"schedule_id" gorm:"type:uuid"`
+	PaymentDate   time.Time          `json:"payment_date"`
+	AmountPaid    float64            `json:"amount_paid"`
+	PaymentMethod string             `json:"payment_method"`
+	PaymentStatus enum.PaymentStatus `json:"payment_status"`
 }
