607abb0a330dd0b05da291dc4c119b4ee881ad50
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-19 00:15:53 +0700

    initialize unit test for payment and payment suite test

---
+++ internal/payment/service/service_test.go
@@ -0,0 +1,12 @@
+package service_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"billing-engine/internal/payment/service"
+)
+
+var _ = Describe("Service", func() {
+
+})
