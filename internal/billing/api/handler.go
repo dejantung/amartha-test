0c522a4fd794d4c0f531573c8936a5e565c3dfce
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-18 20:00:25 +0700

    add create loan handler

---
+++ internal/billing/api/handler.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"billing-engine/internal/billing/model"
+	"billing-engine/pkg/response"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func (s *AppServer) CreateLoanHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	payload := model.CreateLoanPayload{}
+	if err := c.Bind(&payload); err != nil {
+		return err
+	}
+
+	if err := c.Validate(payload); err != nil {
+		return err
+	}
+
+	result, err := s.BillingService.CreateLoan(ctx, payload)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, response.NewSuccessResponse(result))
+}
