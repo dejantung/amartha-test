2444889ed46f475c7e7296c5da9a11a606bc921a
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-18 20:05:33 +0700

    add handler to get is customer deliquent

--- internal/billing/api/handler.go
+++ internal/billing/api/handler.go
@@ -3,6 +3,7 @@ package api
 import (
 	"billing-engine/internal/billing/model"
 	"billing-engine/pkg/response"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
@@ -40,6 +41,24 @@ func (s *AppServer) GetPaymentScheduleHandler(c echo.Context) error {
 	}
 
 	result, err := s.BillingService.GetPaymentSchedule(ctx, payload)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, response.NewSuccessResponse(result))
+}
+
+func (s *AppServer) IsCustomerDelinquentHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	customerID := c.Param("customer_id")
+	// convert string to uuid
+	customerUUID, err := uuid.Parse(customerID)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, response.NewErrorResponse(400, "invalid customer id"))
+	}
+
+	result, err := s.BillingService.IsCustomerDelinquency(ctx, customerUUID)
 	if err != nil {
 		return err
 	}
