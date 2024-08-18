d7f8f11975698c2f963e7d275ca1c80a760bfa05
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-18 09:16:54 +0700

    add method `GetTotalUnpaidPaymentOnActiveLoan` in repository

--- internal/billing/repository/repository.go
+++ internal/billing/repository/repository.go
@@ -4,6 +4,7 @@ import (
 	"billing-engine/internal/billing/domain"
 	"context"
 	"github.com/google/uuid"
+	"time"
 )
 
 //go:generate mockgen -destination=../mocks/mock_billing_repository.go -package=mocks billing-engine/internal/billing/repository BillingRepositoryProvider
@@ -11,8 +12,9 @@ type BillingRepositoryProvider interface {
 	CreateLoan(ctx context.Context, request domain.Loan) (*domain.Loan, error)
 	GetSchedule(ctx context.Context, loanID, customerID uuid.UUID) ([]domain.Schedule, error)
 	GetAllSchedule(ctx context.Context, customerID uuid.UUID) ([]domain.Schedule, error)
-	GetLatestLoan(ctx context.Context, customerID uuid.UUID) (*domain.Loan, error)
+	GetUnpaidAndMissPaymentUntil(ctx context.Context, customerID uuid.UUID, date time.Time) ([]domain.Schedule, error)
 	GetLoanByIDAndCustomerID(ctx context.Context, loanID, customerID uuid.UUID) (*domain.Loan, error)
+	GetTotalUnpaidPaymentOnActiveLoan(ctx context.Context, customerID uuid.UUID) (float64, error)
 
 	// CreateCustomer NOTE: this method is out of context, so I will just merge it in the billing service
 	CreateCustomer(ctx context.Context, request []domain.Customer) error
