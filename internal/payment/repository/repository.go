f7fa2e3d0041ffed33fd6cdefdf38476ce6761a3
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-19 00:14:39 +0700

    add repository and generate mocks

---
+++ internal/payment/repository/repository.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"billing-engine/pkg/enum"
+	"context"
+	"github.com/google/uuid"
+)
+
+//go:generate mockgen -destination=../mocks/mock_payment_repository.go -package=mocks billing-engine/internal/payment/repository PaymentRepositoryProvider
+type PaymentRepositoryProvider interface {
+	IsLoanScheduleExist(ctx context.Context, loanID uuid.UUID, scheduleID uuid.UUID) (bool, error)
+	IsCustomerHasLoan(ctx context.Context, customerID uuid.UUID, loanID uuid.UUID) (bool, error)
+	UpdatePaymentScheduleStatus(ctx context.Context, loanID uuid.UUID, scheduleID uuid.UUID, status enum.PaymentStatus) error
+}
