49e9fc32d0658a994b67739e9c7b1a45e38ea801
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-19 00:05:06 +0700

    add payment struct

---
+++ internal/payment/domain/payment.go
@@ -0,0 +1,17 @@
+package domain
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Payment struct {
+	Base
+	PaymentID     uuid.UUID `json:"payment_id" gorm:"type:uuid;primaryKey"`
+	LoanID        uuid.UUID `json:"loan_id" gorm:"type:uuid"`
+	ScheduleID    uuid.UUID `json:"schedule_id" gorm:"type:uuid"`
+	PaymentDate   time.Time `json:"payment_date"`
+	AmountPaid    float64   `json:"amount_paid"`
+	PaymentMethod string    `json:"payment_method"`
+	PaymentStatus string    `json:"payment_status"`
+}
