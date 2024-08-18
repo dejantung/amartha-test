b3ddf08d044bf9b4d2c314f1b5910845d0049304
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-18 20:00:07 +0700

    add base response helper

---
+++ pkg/response/base.go
@@ -0,0 +1,19 @@
+package response
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func NewResponse(code int, message string, data interface{}) Response {
+	return Response{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	}
+}
+
+func NewSuccessResponse(data interface{}) Response {
+	return NewResponse(200, "Success", data)
+}
