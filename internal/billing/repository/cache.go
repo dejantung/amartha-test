7aeeb49d8a9148de62abde8c5b4cd66f0ad2757f
Dezan Andhika <dezanandhikaa@gmail.com>
2024-08-18 23:26:58 +0700

    fixing cache set to convert to json first

--- internal/billing/repository/cache.go
+++ internal/billing/repository/cache.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"billing-engine/pkg/logger"
 	"context"
+	"encoding/json"
+	"errors"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -21,7 +23,8 @@ func (r redisCache) Set(ctx context.Context, key string, value interface{}) erro
 	r.log.WithField("key", key).
 		WithField("value", value).Info("[Set] setting key to cache")
 
-	err := r.client.Set(ctx, key, value, 0).Err()
+	valJson, _ := json.Marshal(value)
+	err := r.client.Set(ctx, key, valJson, 0).Err()
 	if err != nil {
 		r.log.WithField("error", err).Error("[Set] failed to set key to cache")
 		return err
@@ -35,17 +38,20 @@ func (r redisCache) Get(ctx context.Context, key string) (interface{}, error) {
 	r.log.WithField("key", key).Info("[Get] getting key from cache")
 
 	val, err := r.client.Get(ctx, key).Result()
-	if err != nil {
+	if err != nil && !errors.Is(err, redis.Nil) {
 		r.log.WithField("error", err).Error("[Get] failed to get key from cache")
 		return nil, err
+	} else if err != nil && errors.Is(err, redis.Nil) {
+		return nil, nil
 	}
 
 	r.log.WithField("key", key).Info("[Get] key retrieved from cache")
 	return val, nil
 }
 
-func NewBillingCacheProvider(client *redis.Client) BillingCacheProvider {
+func NewBillingCacheProvider(client *redis.Client, log logger.Logger) BillingCacheProvider {
 	return &redisCache{
 		client: client,
+		log:    log,
 	}
 }
